8b4676b4e2d1f961418c963498ea0db053dbe937
Suvendu-Kre <suvendu.b@krelixir.com>
2026-04-17 16:58:42 +0530

    Add src/components/TodoForm.js

---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './TodoForm.css';
+
+function TodoForm({ addTodo }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(inputValue);
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new todo..."
+        className="todo-input"
+      />
+      <button type="submit" className="todo-button">Add Todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
